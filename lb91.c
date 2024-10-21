f0e9b3c1f066c71b023be06ace56d0f47e2e921d
Ugriumova <vikaezhevika1408@gmail.com>
2024-10-21 12:17:25 +0300

    Add files via upload

---
+++ lb91.c
@@ -0,0 +1,113 @@
+﻿#define ROW 9
+#define COL 9
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES 
+#include <stdio.h> 
+#include <locale.h>  
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	//задание 1.1
+	int row, col;
+	printf("\n");
+	for (row = 1; row <= ROW; ++row)
+	{
+		for (col = 1; col <= COL; col++)
+		{
+			printf("%5d", col * row);
+			printf("\n");
+		}
+	}
+	//задание 1.2
+	printf("\n");
+	printf("\n");
+	int x, y;
+	for (x = 2; x <= 2; x++)
+	{
+		for (y = 1; y <= 9; y++)
+		{
+			printf("%5d", x * y);
+			printf("\n");
+		}
+	}
+	for (x = 6; x <= 6; x++)
+	{
+		for (y = 1; y <= 9; y++)
+		{
+			printf("%5d", x * y);
+			printf("\n");
+		}
+	}
+	for (x = 7; x <= 7; x++)
+
+	{
+		for (y = 1; y <= 9; y++)
+		{
+			printf("%5d", x * y);
+			printf("\n");
+		}
+	}
+	//задание 1.3
+	int a, b;
+	int n = 5;
+	printf("\n");
+	printf("\n");
+	for (a = 1; a <= 5; a++)
+	{
+		if (a == 1)
+		{
+			printf("%5d", n * a);
+			printf("%5d", n * a);
+			printf("%5d", n * a);
+			printf("%5d", n * a);
+			printf("%5d", n * a);
+		}
+		if (a == 2)
+		{
+			printf("%5d", n * a);
+			printf("%5d", n * a);
+			printf("%5d", n * a);
+			printf("%5d", n * a);
+		}
+		if (a == 3)
+		{
+			printf("%5d", n * a);
+			printf("%5d", n * a);
+			printf("%5d", n * a);
+		}
+		if (a == 4)
+		{
+			printf("%5d", n * a);
+			printf("%5d", n * a);
+		}
+		if (a == 5)
+			printf("%5d", n * a);
+
+
+		printf("\n");
+
+	}
+	//задание 2
+
+	while (1)
+	{
+		int s = 0;
+		puts("Введите число");
+		scanf_s("%d", &a);
+		while ((int)a > 0)
+		{
+			(int)n = a % 10;
+			a = a / 10;
+			if (n % 2 == 0) s = s + 1;
+		}
+		printf("%d\n", s);
+
+		printf("Продолжить ? (Да - y, нет - n)");
+		if ((a = getchar()) == 'n') break;
+
+	}
+
+
+
+	system("pause");
+}
